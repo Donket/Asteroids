27b486150a553ea331756e1296ef38064940ab3e
Paul Eastlund <paul.duncan.e@gmail.com>
2026-01-30 21:34:49 -0500

    Finished level scaling (besides balancing) and fixed residual dev-mode bug

--- mainScene/scripts/global.gd
+++ mainScene/scripts/global.gd
@@ -3,7 +3,7 @@ extends Camera2D
 var asteroidsDeck = [null, null, null, null, null, null]
 #stats = [+speed, +damage] where nums are added to base stats 
 var asteroidPermStats = [[0,0], [0,0], [0,0], [0,0], [0,0], [0,0]]
-var asteroidExps = [20,0,0,0,0,0]
+var asteroidExps = [0,0,0,0,0,0]
 var starsDeck = []
 var itemGrabbed = null
 var overSell = false
